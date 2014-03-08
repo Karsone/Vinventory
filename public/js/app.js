9720bf1082bea601c0a281a58ce622f1216802a1
Karsone <hello@karsonenns.com>
2014-03-08 12:16:38 -0800

    Finished Consumption Table

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
-var vinventory = angular.module('vinventory', ['ngTouch','ngRoute']);
+var vinventory = angular.module('vinventory', ['ngTouch','ngRoute','ngResource']).
+	run(function(productService){
+		productService.products();
+	});
 
 //Global Data
 vinventory.factory("globalData", function(){
@@ -22,22 +25,35 @@ vinventory.factory("globalFunctions", function($http, $rootScope, $location, glo
 	// var function = function(){
 	// }
 
-	var http = function(url, data, callback){
-		$http({method: 'GET', url: url, params:data})
-			.then(function(response){
-				//Success
+	var http = {
 
-				var response = angular.fromJson(response.data);
+		get: function(url, data, callback){
+			$http({method: 'GET', url: url, params:data})
+				.then(function(response){
+					//Success
+
+					var response = angular.fromJson(response.data);
+					if (response.isSuccessful == 1) {
+						(callback)(response);
+					} else {
+						alert("ERROR");
+					}
+				}, function(response){
+					//Error
+					alert("BIG ERROR");
+				})
+		},
+		post: function(url, data, callback){
+
+			$http.post(url, data).success(function(response){
 				if (response.isSuccessful == 1) {
 					(callback)(response);
 				} else {
 					alert("ERROR");
 				}
-			}, function(response){
-				//Error
-				alert("BIG ERROR");
-			})
-		;
+			});
+
+		}
 	}
 
 	return{
