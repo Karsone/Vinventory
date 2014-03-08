92c4a1109e62e866b2e8e17c1959a3a0103dd7a3
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2014-03-08 11:54:47 -0800

    Added count to products

--- routes/product.js
+++ routes/product.js
@@ -69,6 +69,7 @@ exports.list = function(db){
 			"name": null,
 			"imageURL": null,
 			"unit": 1,
+			"count": 0,
 			"abuseMessage": null,
 			"isAlcoholic": 0,
 		}
@@ -84,6 +85,10 @@ exports.list = function(db){
 						newProduct[key] = productData[key];
 					}
 				case "isAlcoholic":
+					if(productData[key].length > 0){
+						newProduct[key] = productData[key];
+					}
+				case "count":
 					if(productData[key].length > 0){
 						newProduct[key] = productData[key];
 					}
@@ -146,63 +151,7 @@ exports.edit = function(db){
 					if(productData[key].length > 0){
 						updatedProduct[key] = productData[key];
 					}
-				case "abuseMessage":
-					if(productData[key].length > 0){
-						updatedProduct[key] = productData[key];
-					}
-				default:
-					if(productData[key].length > 0){
-						updatedProduct[key] = productData[key];
-					} else {
-						data.isSuccessful = 0;
-						data.alertLevel = "dataError";
-						data.alertMessages.push(key + " is required");
-					}
-			}
-		}
-
-		db.collection('products').update({"_id": new ObjectID(req.params.id)}, { $set: updatedProduct }, function(err, item){
-			if(err){
-				data.isSuccessful = 0;
-				data.alertLevel = err;
-				data.alertMessages = err;
-			}
-			data.isSuccessful = 1;
-			data.product = item;
-			data.products = exports.list(db);
-
-			res.json(data);
-		});
-	}
-}
-
-/*
- * PUT updated product data
- */
-exports.edit = function(db){
-	return function(req, res){
-
-		var data = {
-			isSuccessful: 0,
-			alertLevel: null,
-			alertMessages: [],
-			product : null
-		}
-
-		var productData = req.body;
-		var updatedProduct = {};
-
-		for (key in productData){
-			switch (key){
-				case "categoryIDs":
-					if(productData[key].length > 0){
-						updatedProduct[key] = productData[key];
-					}
-				case "unit":
-					if(productData[key].length > 0){
-						updatedProduct[key] = productData[key];
-					}
-				case "isAlcoholic":
+				case "count":
 					if(productData[key].length > 0){
 						updatedProduct[key] = productData[key];
 					}
