6a7f42bc1a7adf743d90c661b94804f814028238
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2014-03-08 11:30:17 -0800

    Added edit product

--- routes/product.js
+++ routes/product.js
@@ -59,7 +59,7 @@ exports.list = function(db){
 		var data = {
 			isSuccessful: 0,
 			alertLevel: null,
-			alertMessages: null,
+			alertMessages: [],
 			product : null
 		}
 
@@ -117,66 +117,60 @@ exports.list = function(db){
 };
 
 /*
- * PUT update product
+ * PUT updated product data
  */
- // exports.update = function(db){
-// 	return function(req, res){
-// 		var data = {
-// 			isSuccessful: 0,
-// 			alertLevel: null,
-// 			alertMessages: null,
-// 			product : null
-// 		}
+exports.edit = function(db){
+	return function(req, res){
 
-// 		var productData = req.body;
-// 		var newProduct = {
-// 			"categoryIDs": [],
-// 			"name": null,
-// 			"imageURL": null,
-// 			"unit": 1,
-// 			"abuseMessage": null,
-// 			"isAlcoholic": 0,
-// 		}
+		var data = {
+			isSuccessful: 0,
+			alertLevel: null,
+			alertMessages: [],
+			product : null
+		}
 
-// 		for (key in productData){
-// 			switch (key){
-// 				case "categoryIDs":
-// 					if(newProduct[key].length > 0){
-// 						newProduct[key] = productData[key];
-// 					}
-// 				case "unit":
-// 					if(newProduct[key].length > 0){
-// 						newProduct[key] = productData[key];
-// 					}
-// 				case "isAlcoholic":
-// 					if(newProduct[key].length > 0){
-// 						newProduct[key] = productData[key];
-// 					}
-// 				case "abuseMessage":
-// 					if(newProduct[key].length > 0){
-// 						newProduct[key] = productData[key];
-// 					}
-// 				default:
-// 					if(productData[key].length > 0){
-// 						newProduct[key] = productData[key];
-// 					} else {
-// 						data.isSuccessful = 0;
-// 						data.alertLevel = "dataError";
-// 						data.alertMessages.push(key + " is required");
-// 					}
-// 			}
-// 		}
-		
-// 		db.collection('products').save(newUser, {safe:true}, function(err, item){
-// 			if(err){
-// 				data.isSuccessful = 0;
-// 				data.alertLevel = err;
-// 				data.alertMessages = err;
-// 			}
-// 			data.isSuccessful = 1;
-// 			data.product = item;
+		var productData = req.body;
+		var updatedProduct = {};
 
-// 			res.json(data);
-// 		});
-// 	}
-// };
+		for (key in productData){
+			switch (key){
+				case "categoryIDs":
+					if(productData[key].length > 0){
+						updatedProduct[key] = productData[key];
+					}
+				case "unit":
+					if(productData[key].length > 0){
+						updatedProduct[key] = productData[key];
+					}
+				case "isAlcoholic":
+					if(productData[key].length > 0){
+						updatedProduct[key] = productData[key];
+					}
+				case "abuseMessage":
+					if(productData[key].length > 0){
+						updatedProduct[key] = productData[key];
+					}
+				default:
+					if(productData[key].length > 0){
+						updatedProduct[key] = productData[key];
+					} else {
+						data.isSuccessful = 0;
+						data.alertLevel = "dataError";
+						data.alertMessages.push(key + " is required");
+					}
+			}
+		}
+
+		db.collection('products').update({"_id": new ObjectID(req.params.id)}, { $set: updatedProduct }, function(err, item){
+			if(err){
+				data.isSuccessful = 0;
+				data.alertLevel = err;
+				data.alertMessages = err;
+			}
+			data.isSuccessful = 1;
+			data.product = item;
+
+			res.json(data);
+		});
+	}
+}
