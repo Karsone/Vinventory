6762d4d06b7703dc297e1ccbda948e068f86e4e1
Lucas Klaassen <lucas@vin65.com>
2014-03-08 12:10:20 -0800

    Added Product Consumption Decrement

--- routes/consumption.js
+++ routes/consumption.js
@@ -88,8 +88,19 @@ exports.list = function(db){
 			}
 			data.isSuccessful = 1;
 			data.consumption = item;
+		});
 
+		db.collection('products').update({ "_id": new ObjectID(req.body.productID)},{ $inc : { count: -1 } }, function(err, item){
+			if(err){
+				data.isSuccessful = 0;
+				data.alertLevel = err;
+				data.alertMessages = err;
+			}
+			data.isSuccessful = 1;
+			data.product = item;
+			// data.products = exports.list(db);
 			res.json(data);
 		});
+
 	}
 };
