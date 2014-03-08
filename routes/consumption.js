4c191fdfece52cdf3280613930ee0c23d6ee6e36
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2014-03-08 14:31:02 -0800

    Return error and don't decrement if count is 0

--- routes/consumption.js
+++ routes/consumption.js
@@ -79,6 +79,27 @@ exports.list = function(db){
 			alertMessages: null,
 			consumption : null
 		}
+
+		db.collection('products').find({ "_id": new ObjectID(req.body.productID)}).toArray(function(err, product) { 
+			if(product[0].count > 0) {
+				db.collection('products').update({ "_id": new ObjectID(req.body.productID)},{ $inc : { count: -1 } }, function(err, item){
+					if(err){
+						data.isSuccessful = 0;
+						data.alertLevel = err;
+						data.alertMessages = err;
+					}
+					data.isSuccessful = 1;
+					data.product = item;
+					// data.products = exports.list(db);
+					// res.json(data);
+				});
+			} else {
+				data.isSuccessful = 0;
+				data.alertLevel = "Error";
+				data.alertMessage = "Product out of inventory";
+				res.json(data);
+			}
+        })  
 		
 		db.collection('consumptions').insert(req.body, function(err, item){
 			if(err){
@@ -89,21 +110,5 @@ exports.list = function(db){
 			data.isSuccessful = 1;
 			data.consumption = item;
 		});
-
-		// Check if product has count of 0
-		// var product = db.collection('products').find({ "_id": new ObjectID(req.body.productID)});
-
-		db.collection('products').update({ "_id": new ObjectID(req.body.productID)},{ $inc : { count: -1 } }, function(err, item){
-			if(err){
-				data.isSuccessful = 0;
-				data.alertLevel = err;
-				data.alertMessages = err;
-			}
-			data.isSuccessful = 1;
-			data.product = item;
-			// data.products = exports.list(db);
-			res.json(data);
-		});
-
 	}
 };
--- routes/product.js
+++ routes/product.js
@@ -11,7 +11,7 @@ exports.list = function(db){
 			alertMessages: [],
 			products : null
 		}
-		db.collection('products').find().toArray(function(err, items){
+		db.collection('products').find({}, { 'sort': 'displayOrder' }).toArray(function(err, items){
 			if(err){
 				data.isSuccessful = 0;
 				data.alertLevel = err;
